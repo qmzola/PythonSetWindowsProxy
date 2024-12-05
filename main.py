c38f94e21f34a5648e05591694e353ab8c2a42f7
Luo <luolinghua1222@outlook.com>
2024-12-05 11:04:35 +0800

    去掉Dns服务器修改相关，实现了Proxy服务器的自动修改

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import winreg
-import os
-import socket
 
+Proxy_Adderss= "192.168.37.25:20000"
+ProxyRegistryPath=r'Software\Microsoft\Windows\CurrentVersion\Internet Settings'
 
-ProxyAdders= "192.168.37.25:20000"
-DnsServerAdders= '192.168.1.1'
-ProxyCommand= 'netsh interface ip set dns name="以太网" static ' + DnsServerAdders + ' primary'
-
-print(ProxyCommand)
-'''
-ProxyKey=winreg.OpenKey("HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings", "ProxyEnable")
-if ProxyKey=0
-    winreg.FlushKey(ProxyKey)
-'''
+try:
+    ProxyEnable=winreg.OpenKey(winreg.HKEY_CURRENT_USER,ProxyRegistryPath)
+    ProxyEnableKey,_=winreg.QueryValueEx(ProxyEnable,'ProxyEnable')
+    if ProxyEnableKey == 0:
+        winreg.SetValueEx(ProxyEnable,'ProxyEnable',0,winreg.REG_DWORD,1)
+        winreg.SetValueEx(ProxyEnable,"ProxyServer", 0, winreg.REG_SZ, Proxy_Adderss)
+        print("代理已开启，服务器地址是" + Proxy_Adderss)
+        winreg.CloseKey(ProxyEnable)
+    elif ProxyEnableKey == 1:
+        winreg.SetValueEx(ProxyEnable,'ProxyEnable',0,winreg.REG_DWORD,0)
+        winreg.DeleteValue(ProxyEnable,"ProxyServer")
+        print("代理已关闭，服务器地址是")
+        winreg.CloseKey(ProxyEnable)
+except FileNotFoundError:
+    print("无法找到指定的注册表项，请检查路径是否正确。")
+except PermissionError:
+    print("权限不足，无法修改注册表，请以管理员权限运行脚本。")
+except Exception as e:
+    print(f"发生未知错误：{e}")
+finally:
+    winreg.CloseKey(ProxyEnable)
